6a6346ebd420175c96a474cfce2a083f0937f87a
Kimlle-88 <kle65@live.spcollege.edu>
2025-10-15 14:41:06 -0400

    roll_dice.py

--- roll_dice.py
+++ roll_dice.py
@@ -2,17 +2,12 @@ import random
 while True:
  choice = input('Roll a dice?')
  if choice.lower() == 'y' or choice.lower() =='yes':
-   d1 = random.radiant(1, 6)
-   d2 = random.radiant(1, 6)
-   print('f'({d1},{d2}))
+   d1 = random.randintt(1, 6)
+   d2 = random.randint(1, 6)
+   print(f'({d1}, {d2})')
 elif choice.lower() =='n' or choice.lower() =='no':
     print('Thanks for playing')
     break
 else:
     print('NO VALID')
-restart = input('Want to start again')
-if restart.lower() != 'y' and restart.lower() != 'yes':
-  print('END')
-  break
-
 
